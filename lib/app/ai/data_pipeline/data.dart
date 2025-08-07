05832e13b8b87809cd63025c4dd95dccf02cf02c
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-08-07 21:42:55 +0530

    data pipeline init

---
+++ lib/app/ai/data_pipeline/data.dart
@@ -0,0 +1,12 @@
+// data pipeline for ai
+
+import 'package:flutter/material.dart';
+
+class DataPipeline extends StatelessWidget {
+  const DataPipeline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
